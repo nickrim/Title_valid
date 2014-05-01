2fcf9fbf892cab390bc7b87a61391a0d0b978fcc
nickrim <anisim@gmail.com>
2014-05-01 14:47:24 -0700

    HW 33 commit

---
+++ src/main/java/Title_valid/App.java
@@ -0,0 +1,13 @@
+package Title_valid;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
